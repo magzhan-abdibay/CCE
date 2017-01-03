5eab9cce657c59d9ec9dffe091fb7390a6fddba8
magzhan-abdibay <abdibaevmagzhan@gmail.com>
2017-01-04 00:55:34 +0600

    unused inputs deleted

--- Source/CCE/Agent.cpp
+++ Source/CCE/Agent.cpp
@@ -1,20 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "CCE.h"
 #include "Agent.h"
 #include "Kismet/HeadMountedDisplayFunctionLibrary.h"
 
-//////////////////////////////////////////////////////////////////////////
-// AAgent
 
 AAgent::AAgent()
 {
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
-
-	// set our turn rates for input
-	BaseTurnRate = 45.f;
-	BaseLookUpRate = 45.f;
 
 	// Don't rotate when the controller rotates. Let that just affect the camera.
 	bUseControllerRotationPitch = false;
@@ -27,23 +19,19 @@ AAgent::AAgent()
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
 
+	/*
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
-												// Create a follow camera
+	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
-												   // Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-												   // are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
+	*/
 }
-
-//////////////////////////////////////////////////////////////////////////
-// Input
 
 void AAgent::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
 {
@@ -55,55 +43,10 @@ void AAgent::SetupPlayerInputComponent(class UInputComponent* PlayerInputCompone
 	PlayerInputComponent->BindAxis("MoveForward", this, &AAgent::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &AAgent::MoveRight);
 
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+	// Rotation bindings
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &AAgent::TurnAtRate);
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &AAgent::LookUpAtRate);
-
-	// handle touch devices
-	PlayerInputComponent->BindTouch(IE_Pressed, this, &AAgent::TouchStarted);
-	PlayerInputComponent->BindTouch(IE_Released, this, &AAgent::TouchStopped);
-
-	// VR headset functionality
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AAgent::OnResetVR);
-}
-
-
-void AAgent::OnResetVR()
-{
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
-}
-
-void AAgent::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
-{
-	// jump, but only on the first touch
-	if (FingerIndex == ETouchIndex::Touch1)
-	{
-		Jump();
-	}
-}
-
-void AAgent::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
-{
-	if (FingerIndex == ETouchIndex::Touch1)
-	{
-		StopJumping();
-	}
-}
-
-void AAgent::TurnAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
-}
-
-void AAgent::LookUpAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+		
 }
 
 void AAgent::MoveForward(float Value)
