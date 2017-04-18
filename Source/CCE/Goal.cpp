9cc42090ec753a3d7453058984901a803128d842
magzhan-abdibay <abdibaevmagzhan@gmail.com>
2017-04-18 23:17:29 +0600

    trigger volume created

---
+++ Source/CCE/Goal.cpp
@@ -0,0 +1,45 @@
+#include "CCE.h"
+#include "Goal.h"
+
+AGoal::AGoal()
+{
+	
+	PrimaryActorTick.bCanEverTick = true;
+	box= CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	box->bGenerateOverlapEvents = true;
+	box->SetRelativeScale3D(FVector(2, 2, 5));
+	RootComponent = box;
+	light = CreateDefaultSubobject<UPointLightComponent>(TEXT("light"));
+	light->Intensity = 10000;
+	light->SetLightColor(FColor::Red);
+	
+	light->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+
+	box->OnComponentBeginOverlap.AddDynamic(this, &AGoal::TriggerEnter);
+	box->OnComponentEndOverlap.AddDynamic(this, &AGoal::TriggerExit);
+	
+}
+
+void AGoal::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AGoal::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+void AGoal::TriggerEnter(UPrimitiveComponent * HitComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	light->SetLightColor(FColor::Green);
+}
+
+void AGoal::TriggerExit(class UPrimitiveComponent* HitComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	light->SetLightColor(FColor::Yellow);
+}
+
+
---
+++ Source/CCE/Goal.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "UObjectGlobals.h"
+#include "Goal.generated.h"
+
+
+UCLASS()
+class CCE_API AGoal : public AActor
+{
+	GENERATED_BODY()
+
+public:	
+	AGoal();
+
+	virtual void BeginPlay() override;
+	
+	
+	virtual void Tick( float DeltaSeconds ) override;
+
+protected:
+	TSubobjectPtrDeprecated<UBoxComponent> box;
+	TSubobjectPtrDeprecated<UPointLightComponent> light;
+
+	UFUNCTION()
+		void TriggerEnter(class UPrimitiveComponent* HitComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	UFUNCTION()
+		void TriggerExit(class UPrimitiveComponent* HitComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+};
