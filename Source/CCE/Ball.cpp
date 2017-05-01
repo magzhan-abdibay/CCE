691d586b44bc6ae9bb7225b1bb804f11c7bc5866
magzhan-abdibay <abdibaevmagzhan@gmail.com>
2017-05-01 13:11:45 +0600

    TMP: pole balancng executed in UE4

--- Source/CCE/Ball.cpp
+++ Source/CCE/Ball.cpp
@@ -1,6 +1,7 @@
 #include "CCE.h"
 #include "Ball.h"
 #include "Population.h"
+#include "experiments.h"
 
 ABall::ABall() {
   PrimaryActorTick.bCanEverTick = true;
@@ -9,10 +10,22 @@ ABall::ABall() {
   RootComponent = Mesh;
 }
 
-void ABall::BeginPlay() { Super::BeginPlay(); }
+void ABall::BeginPlay() { 
+	Super::BeginPlay(); 
+	
+	DoStuff(); 
+}
 
 void ABall::Tick(float DeltaTime) { Super::Tick(DeltaTime); }
 
 void ABall::DoStuff() {
+	GEngine->AddOnScreenDebugMessage(-1, 7.f, FColor::Blue,"start");
+	char* path = "E:\\UnrealProjects\\CCE\\NEAT\\NEATConsoleApplication\\pole2_markov.ne";
+	NEAT::loadNeatParams(path, true);
+
 	NEAT::Population *p = 0;
+	p = pole2TestRealTime();
+	if (p) {
+		delete p;
+	}
 }
